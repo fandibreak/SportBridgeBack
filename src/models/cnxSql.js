d589e63c2ea19d45a19f50a9954cb9db48d1acc2
fandibreak <jalid.acdhi@gmail.com>
2022-01-09 20:46:07 +0100

    last Update

--- src/models/cnxSql.js
+++ src/models/cnxSql.js
@@ -3,24 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'eclkveco',
     host: 'tai.db.elephantsql.com',
-    database: 'eclkveco',
-    password: 'F1PJGENL8Q-hcXCjVKklydwOP--I89WG'
-    // database: process.env.DATABASE,
-    // password: process.env.DB_PASSWORD
-//Apikey : 613a2d25-7e54-47a5-a1b4-0b2ca41b0cfd (por si se debe usar)
+    database: process.env.DATABASE,
+    password: process.env.DB_PASSWORD
+
 })
 
 module.exports = pool;
 
-/*
-const mongoose = require('../utils/mgBBDD')
-
-const product = {
-    getProductById,
-    getAllProducts,
-    createProduct
-}
-
-module.exports = product;
-
-*/
